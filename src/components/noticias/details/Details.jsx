7c2855b1b5e350b2fc2227e7e80712c85e85ca6b
kevin <kevinbrrs96@gmail.com>
2024-06-22 14:06:45 -0300

    Feat(details)= se modifico stylos de details, se agrego un button para volver a noticias

--- src/components/noticias/details/Details.jsx
+++ src/components/noticias/details/Details.jsx
@@ -1,5 +1,5 @@
-import { useParams } from "react-router-dom";
-import stylos from "./details.module.css";
+import { Link, useParams } from "react-router-dom";
+import styles from "./details.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getPostId } from "../../../redux/actions";
@@ -13,13 +13,36 @@ function Details() {
     dispatch(getPostId(id));
   }, [dispatch, id]);
 
+  if (!postDetails || !postDetails.description) {
+    return <div>Loading...</div>;
+  }
+
+  const descriptionParagraphs = postDetails.description
+    .split("\n")
+    .map((paragraph, index) => <p key={index}>{paragraph}</p>);
+
   return (
-    <div className={stylos.container}>
-      <div className={stylos.hero}>
+    <div className={styles.container}>
+      <div className={styles.hero}>
         <h1>{postDetails.title}</h1>
         <h2>{postDetails.subtitle}</h2>
         <img src={postDetails.image} alt={postDetails.title} />
-        <p>{postDetails.description}</p>
+        {descriptionParagraphs}
+        <Link to="/noticias">
+          <button>
+            <svg
+              height="16"
+              width="16"
+              color="#fff"
+              xmlns="http://www.w3.org/2000/svg"
+              version="1.1"
+              viewBox="0 0 1024 1024"
+            >
+              <path d="M874.690416 495.52477c0 11.2973-9.168824 20.466124-20.466124 20.466124l-604.773963 0 188.083679 188.083679c7.992021 7.992021 7.992021 20.947078 0 28.939099-4.001127 3.990894-9.240455 5.996574-14.46955 5.996574-5.239328 0-10.478655-1.995447-14.479783-5.996574l-223.00912-223.00912c-3.837398-3.837398-5.996574-9.046027-5.996574-14.46955 0-5.433756 2.159176-10.632151 5.996574-14.46955l223.019353-223.029586c7.992021-7.992021 20.957311-7.992021 28.949332 0 7.992021 8.002254 7.992021 20.957311 0 28.949332l-188.073446 188.073446 604.753497 0C865.521592 475.058646 874.690416 484.217237 874.690416 495.52477z"></path>
+            </svg>
+            <span>Volver</span>
+          </button>
+        </Link>
       </div>
     </div>
   );
